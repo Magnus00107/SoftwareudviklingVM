2bc3d92ecde6965f4d54bc88c2858dd318e7d644
MagnusUbuntu <magnusmeldgaard14@gmail.com>
2025-05-22 19:50:54 +0200

    Hero klassens header-fill er konstrueret.

--- SU_Project/header/hero.hpp
+++ SU_Project/header/hero.hpp
@@ -4,17 +4,36 @@ using namespace std;
 class Hero {
     public:
 
+    //Service functions
     Hero(const string& name);
 
     void attack(class Enemy& enemy);
 
-    
+    void takeDamage(int dmg);
 
+    void changeXP(int xp);
+
+    void levelUp();
+
+    void isAlive() const;
+
+    //Getter functions
+    string getName() const;
+
+    int getLevel();
+
+    int getXp();
+
+    int getHp();
+
+    int getAtkDmg();
     
     private:
-    string name;
-    int xp;
-    int level;
-    int hp;
-    int atkdmg
-}
+        string name;
+        int xp;
+        int level;
+        int hp;
+        int atkdmg;
+};
+
+//
